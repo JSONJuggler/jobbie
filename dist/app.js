09ac40d1f8c652cb57017af6e2e6f6dcade56960
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-08-21 04:57:48 -0500

    fixes and adjustments for production

--- dist/app.js
+++ dist/app.js
@@ -4,8 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
 const app = express_1.default();
-app.get("/", (req, res, next) => {
-    res.send("Hello");
-});
-app.listen(5000, () => console.log("Server running"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.use(express_1.default.json());
+//Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+    // Set static folder
+    app.use(express_1.default.static(path_1.default.join(__dirname, "client", "build")));
+    // Serve index.html on all routes except any api routes above
+    app.get("*", function (req, res) {
+        res.sendFile(path_1.default.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
--- src/app.ts
+++ src/app.ts
@@ -11,10 +11,10 @@ app.use(express.json());
 //Serve static assets in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static(path.join(__dirname, "client", "build")));
+  app.use(express.static(path.join(__dirname, "../client", "build")));
 
   // Serve index.html on all routes except any api routes above
   app.get("*", function (req, res) {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
