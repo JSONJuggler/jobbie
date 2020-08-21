f25306a8a88564a0edaef69a9dae1d27f7d473d5
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-08-21 15:10:43 -0500

    puppeteer getting window dimensions (also using Dimensions interface for typing)

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,45 @@
-import express, { Application, Request, Response, NextFunction } from "express";
-import path from "path";
+//import express, { Application, Request, Response, NextFunction } from "express";
+//import path from "path";
+import puppeteer, { Page, Browser } from "puppeteer";
 
-const app: Application = express();
-const PORT = process.env.PORT || 5000;
+//const app: Application = express();
+//const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+//app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-app.use(express.json());
+//app.use(express.json());
 
-//Serve static assets in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static(path.join(__dirname, "../client", "build")));
+////Serve static assets in production
+//if (process.env.NODE_ENV === "production") {
+//// Set static folder
+//app.use(express.static(path.join(__dirname, "../client", "build")));
 
-  // Serve index.html on all routes except any api routes above
-  app.get("*", function (req, res) {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-  });
+//// Serve index.html on all routes except any api routes above
+//app.get("*", function (req, res) {
+//res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+//});
+//}
+
+interface Dimensions {
+  width: number;
+  height: number;
+  deviceScaleFactor: number;
 }
+
+(async () => {
+  const browser: Browser = await puppeteer.launch();
+  const page: Page = await browser.newPage();
+  await page.goto("https://en.wikipedia.org/wiki/COVID-19_pandemic_in_Texas");
+  //await page.screenshot({ path: "woot.png" });
+  //await browser.waitForTarget(() => false);
+  const result: Dimensions = await page.evaluate(() => {
+    return {
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight,
+      deviceScaleFactor: window.devicePixelRatio,
+    };
+  });
+
+  console.log("Dimensions:", result);
+  await browser.close();
+})();
