22bfa2cb98f49ce08ff0a67198dbdc130059f556
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-08-21 04:08:48 -0500

    production test

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,20 @@
 import express, { Application, Request, Response, NextFunction } from "express";
+import path from "path";
 
 const app: Application = express();
+const PORT = process.env.PORT || 5000;
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("Hello");
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-app.listen(5000, () => console.log("Server running"));
+app.use(express.json());
+
+//Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static(path.join(__dirname, "client", "build")));
+
+  // Serve index.html on all routes except any api routes above
+  app.get("*", function (req, res) {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
