d8d39f8abdfe0a90c2d8c36ebe1dcaf4c24420ff
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-08-23 16:30:39 -0500

    logging into indeed and then using those cookies in a new browser to not have to log in again. tried writing cookies to a file but that breaks typing and typescript doesnt like that. will have to address this so that cookies can be loaded from a file or maybe a better approach is to just store the cookies in the user browser

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express, { Application, Request, Response, NextFunction } from "express";
 import path from "path";
 import puppeteer, { Page, Browser, Target, ConsoleMessage } from "puppeteer";
+import fs from "fs";
+//import cookies from "../cookies.json";
 
 const app: Application = express();
 const PORT = process.env.PORT || 5000;
@@ -42,7 +44,7 @@ if (process.env.NODE_ENV === "production") {
 
 (async (): Promise<void> => {
   const browser: Browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
     args: ["--window-size=400,600"],
   });
   const page: Page = await browser.newPage();
@@ -59,18 +61,72 @@ if (process.env.NODE_ENV === "production") {
     console.log("PAGE LOG:", msg.text())
   );
 
+  // check if we have a previously saved session
+  //if (!Object.keys(cookies).length) {
+  if (false) {
+    //console.log(Object.keys(cookies).length);
+    // set the saved cookes in the puppeteer browser page
+    //await page.setCookie(...cookies);
+    console.log("here");
+    await page.goto("https://www.indeed.com/");
+  } else {
+    const qbrowser: Browser = await puppeteer.launch({
+      headless: false,
+      args: ["--window-size=400,600"],
+    });
+    const qpage: Page = await qbrowser.newPage();
+    await qpage.setViewport({
+      width: 500,
+      height: 600,
+      deviceScaleFactor: 1,
+    });
+    qbrowser.on(
+      "targetdestroyed",
+      async (): Promise<void> => await qbrowser.close()
+    );
+    qpage.on("console", (msg: ConsoleMessage): void =>
+      console.log("PAGE LOG:", msg.text())
+    );
+    //await qpage.goto("https://www.indeed.com/");
+    await qpage.goto("https://secure.indeed.com/account/login");
+
+    // careful, code pauses here if there is no navigation to either of the urls below
+    await qbrowser.waitForTarget(
+      (target: Target): boolean => {
+        console.log(target.url());
+        return (
+          target.url().startsWith("https://www.indeed.com/") ||
+          target.url() === "https://secure.indeed.com/account/view"
+        );
+      },
+      { timeout: 0 }
+    );
+
+    let currentCookies = await qpage.cookies();
+    const qqbrowser: Browser = await puppeteer.launch({
+      headless: false,
+      args: ["--window-size=400,600"],
+    });
+    const qqpage: Page = await qqbrowser.newPage();
+    await qqpage.setCookie(...currentCookies);
+    await qqpage.goto("https://www.indeed.com/");
+    console.log("test");
+
+    //fs.writeFileSync("./cookies.json", JSON.stringify(currentCookies));
+  }
+
   await page.goto("https://secure.indeed.com/account/login");
 
-  await browser.waitForTarget(
-    (target: Target): boolean => {
-      //console.log(target.url());
-      return (
-        target.url().startsWith("https://www.indeed.com/") ||
-        target.url() === "https://secure.indeed.com/account/view"
-      );
-    },
-    { timeout: 0 }
-  );
+  //await browser.waitForTarget(
+  //(target: Target): boolean => {
+  ////console.log(target.url());
+  //return (
+  //target.url().startsWith("https://www.indeed.com/") ||
+  //target.url() === "https://secure.indeed.com/account/view"
+  //);
+  //},
+  //{ timeout: 0 }
+  //);
 
   await page.screenshot({ path: "dist/example.png" });
 
