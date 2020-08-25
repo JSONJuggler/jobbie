0c23159a39324c38db969a47820e5ebde1baa01f
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-08-25 00:13:14 -0500

    typing useState and useEffect

--- client/src/components/Test.tsx
+++ client/src/components/Test.tsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 interface Props {
   text: string;
 }
 
 const Test: React.FC<Props> = ({ text }) => {
-  return <div>{text}</div>;
+  const [count, setCount] = useState<number | null>(1);
+
+  useEffect((): void => {
+    setCount(10);
+  }, []);
+
+  return <div>{text + count}</div>;
 };
 
 export default Test;
