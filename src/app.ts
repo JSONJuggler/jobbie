b9f15788781deef23b27b77d2954436ec81e57ed
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-08-24 21:10:08 -0500

    getting cookies after user logs into indeed and sending in the response headers. having a problem with the user not being logged in with cookies received from puppeteer.

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
 import express, { Application, Request, Response, NextFunction } from "express";
 import path from "path";
-import puppeteer, { Page, Browser, Target, ConsoleMessage } from "puppeteer";
+import puppeteer, {
+  Page,
+  Browser,
+  Target,
+  ConsoleMessage,
+  Cookie,
+} from "puppeteer";
+// @ts-ignore
+import cookie from "cookie";
 import fs from "fs";
-//import cookies from "../cookies.json";
 
 const app: Application = express();
 const PORT = process.env.PORT || 5000;
@@ -11,25 +18,81 @@ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.use(express.json());
 
-app.get("/", async (req: Request, res: Response, next: NextFunction) => {
-  //const browser: Browser = await puppeteer.launch();
-  //const page: Page = await browser.newPage();
-  //await page.goto("https://en.wikipedia.org/wiki/COVID-19_pandemic_in_Texas");
+app.get(
+  "/",
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const browser: Browser = await puppeteer.launch({
+      headless: false,
+      args: ["--window-size=400,600"],
+    });
+    const page: Page = await browser.newPage();
+    await page.setViewport({
+      width: 500,
+      height: 600,
+      deviceScaleFactor: 1,
+    });
+    browser.on(
+      "targetdestroyed",
+      async (): Promise<void> => {
+        res.send("Browser closed");
+        return await browser.close();
+      }
+    );
+    page.on("console", (msg: ConsoleMessage): void =>
+      console.log("PAGE LOG:", msg.text())
+    );
 
-  //page.on("console", (msg) => console.log("PAGE LOG:", msg.text()));
+    await page.goto("https://secure.indeed.com/account/login");
 
-  //const result: Array<string> = await page.evaluate(() => {
-  //let headingsFromPage: NodeListOf<Element> = document.querySelectorAll(
-  //".mw-headline"
-  //);
-  //const headingsList: Array<Element> = [...headingsFromPage];
-  //console.log(`url is ${location.href}`);
-  //return headingsList.map((h) => h.innerHTML);
-  //});
-  //await browser.close();
-  //res.send(result);
-  res.send("Hello");
-});
+    // careful, code pauses here if there is no navigation to either of the urls below
+
+    await browser.waitForTarget(
+      (target: Target): boolean => {
+        //console.log(target.url());
+        return (
+          target.url().startsWith("https://www.indeed.com/") ||
+          target.url() === "https://secure.indeed.com/account/view"
+        );
+      },
+      { timeout: 0 }
+    );
+
+    let currentCookies: Array<Cookie> = await page.cookies();
+
+    //await page.screenshot({ path: "dist/example.png" });
+
+    await browser.close();
+
+    const cookies: Array<string> = currentCookies.map(
+      (currentCookie: Cookie): string => {
+        const {
+          name,
+          value,
+        }: //domain,
+        //path,
+        //expires,
+        //size,
+        //httpOnly,
+        //secure,
+        //session,
+        Cookie = currentCookie;
+        //console.log(new Date(expires));
+        return cookie.serialize(name, value, {
+          //domain,
+          //path,
+          //expires: new Date(expires),
+          //size,
+          //httpOnly,
+          //secure,
+          //session,
+        });
+      }
+    );
+    console.log(cookies);
+    res.setHeader("set-cookie", cookies);
+    res.send("Hello");
+  }
+);
 
 //Serve static assets in production
 if (process.env.NODE_ENV === "production") {
@@ -41,103 +104,3 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
-
-(async (): Promise<void> => {
-  const browser: Browser = await puppeteer.launch({
-    headless: true,
-    args: ["--window-size=400,600"],
-  });
-  const page: Page = await browser.newPage();
-  await page.setViewport({
-    width: 500,
-    height: 600,
-    deviceScaleFactor: 1,
-  });
-  browser.on(
-    "targetdestroyed",
-    async (): Promise<void> => await browser.close()
-  );
-  page.on("console", (msg: ConsoleMessage): void =>
-    console.log("PAGE LOG:", msg.text())
-  );
-
-  // check if we have a previously saved session
-  //if (!Object.keys(cookies).length) {
-  if (false) {
-    //console.log(Object.keys(cookies).length);
-    // set the saved cookes in the puppeteer browser page
-    //await page.setCookie(...cookies);
-    console.log("here");
-    await page.goto("https://www.indeed.com/");
-  } else {
-    const qbrowser: Browser = await puppeteer.launch({
-      headless: false,
-      args: ["--window-size=400,600"],
-    });
-    const qpage: Page = await qbrowser.newPage();
-    await qpage.setViewport({
-      width: 500,
-      height: 600,
-      deviceScaleFactor: 1,
-    });
-    qbrowser.on(
-      "targetdestroyed",
-      async (): Promise<void> => await qbrowser.close()
-    );
-    qpage.on("console", (msg: ConsoleMessage): void =>
-      console.log("PAGE LOG:", msg.text())
-    );
-    //await qpage.goto("https://www.indeed.com/");
-    await qpage.goto("https://secure.indeed.com/account/login");
-
-    // careful, code pauses here if there is no navigation to either of the urls below
-    await qbrowser.waitForTarget(
-      (target: Target): boolean => {
-        console.log(target.url());
-        return (
-          target.url().startsWith("https://www.indeed.com/") ||
-          target.url() === "https://secure.indeed.com/account/view"
-        );
-      },
-      { timeout: 0 }
-    );
-
-    let currentCookies = await qpage.cookies();
-    const qqbrowser: Browser = await puppeteer.launch({
-      headless: false,
-      args: ["--window-size=400,600"],
-    });
-    const qqpage: Page = await qqbrowser.newPage();
-    await qqpage.setCookie(...currentCookies);
-    await qqpage.goto("https://www.indeed.com/");
-    console.log("test");
-
-    //fs.writeFileSync("./cookies.json", JSON.stringify(currentCookies));
-  }
-
-  await page.goto("https://secure.indeed.com/account/login");
-
-  //await browser.waitForTarget(
-  //(target: Target): boolean => {
-  ////console.log(target.url());
-  //return (
-  //target.url().startsWith("https://www.indeed.com/") ||
-  //target.url() === "https://secure.indeed.com/account/view"
-  //);
-  //},
-  //{ timeout: 0 }
-  //);
-
-  await page.screenshot({ path: "dist/example.png" });
-
-  //const result: Array<string> = await page.evaluate(() => {
-  //let headingsFromPage: NodeListOf<Element> = document.querySelectorAll(
-  //".mw-headline"
-  //);
-  //const headingsList: Array<Element> = [...headingsFromPage];
-  //console.log(`url is ${location.href}`);
-  //return headingsList.map((h) => h.innerHTML);
-  //});
-  await browser.close();
-  //console.log("Header Array:", result);
-})();
